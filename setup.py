c834e51d90df2ebaf73602322421fd48900b4ef2
ketanGangal <ketangangal98@gmail.com>
2022-03-22 14:26:53 +0530

    files

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+# Edit below variables as per your requirements -
+REPO_NAME = "level_2_architecture"
+AUTHOR_USER_NAME = "c17hawke"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = []
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for level_2_architecture",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="sunny.c17hawke@gmail.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=LIST_OF_REQUIREMENTS
+)
--- src/utils/get_secrets.py
+++ src/utils/get_secrets.py
@@ -1,10 +1,11 @@
 import os
 import json
 
-def secrets(ENV_VAR = 'AWS_SECRETS'):
+
+def secrets(ENV_VAR='AWS_SECRETS'):
     SECRETS = os.environ.get(ENV_VAR)
-    print(SECRETS)
     return json.loads(SECRETS)
+
 
 if __name__ == '__main__':
     secrets()
